5c7eeaa040a50c3bc57a3d2de25e4a85302b0180
realy509 <85831169+realy509@users.noreply.github.com>
2025-01-27 15:13:07 +0300

    Update BlockBreakListener.java

--- src/main/java/tr/alperendemir/boostXP/listeners/BlockBreakListener.java
+++ src/main/java/tr/alperendemir/boostXP/listeners/BlockBreakListener.java
@@ -17,7 +17,8 @@ public class BlockBreakListener implements Listener {
     private final List<Material> logTypes = Arrays.asList(
             Material.ACACIA_LOG, Material.BIRCH_LOG, Material.DARK_OAK_LOG,
             Material.JUNGLE_LOG, Material.OAK_LOG, Material.SPRUCE_LOG,
-            Material.MANGROVE_LOG, Material.CHERRY_LOG
+            Material.MANGROVE_LOG, Material.CHERRY_LOG,
+            Material.SOUL_SAND
     );
 
     public BlockBreakListener(ExperienceManager experienceManager) {
